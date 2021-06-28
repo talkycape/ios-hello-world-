46df15283f8beb4cad2cbf49b2fa9e5cf9f22e82
Ya-Chieh Lai <yach@alum.mit.edu>
2021-06-27 17:27:32 -0700

    not sure why these didn't get committed first time around...

---
+++ hello world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello world
+//
+//  Created by Sabrina Hsieh on 6/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello world
+//
+//  Created by Sabrina Hsieh on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
