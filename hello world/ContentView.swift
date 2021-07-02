c4f99144fddd1aeb7030a46526dbcb72b5c2c8e4
Ya-Chieh Lai <yachieh@gmail.com>
2021-07-02 01:49:14 -0700

    Doing something more interesting now.  Demonstrate navigation, buttons, images, sounds.

--- hello world/ContentView.swift
+++ hello world/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct splashView: View {
 
@@ -16,8 +17,10 @@ struct splashView: View {
             if self.isActive {
                 ContentView()
             } else {
-                Text("Awesome Splash Screen!")
-                    .font(Font.largeTitle)
+                Image("AwesomeSplash")
+// An alternative Text version of the splash screen
+//                Text("Awesome Splash Screen!")
+//                    .font(Font.largeTitle)
             }
         }
         .onAppear() {
@@ -30,12 +33,89 @@ struct splashView: View {
     }
     
 }
+
 struct ContentView: View {
+    @State private var isPressed = false
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Hello World!")
+                Divider()
+                NavigationLink(destination: myImageView()) {
+                    Text("Now let's follow a link to an image")
+                }
+                Divider()
+                NavigationLink(destination: mySoundView()) {
+                    Text("Now let's follow a link to play a sound")
+                }
+                Divider()
+                Button(action: {
+                    playAudioAsset("myTestSound")
+                  }, label: {
+                      Text("Or use a button to perform an action")
+                  })
+                Divider()
+                HStack(spacing:10) {
+                    Text("Horizontal Content:")
+                    Image("spongebob")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    Image("plankton")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                    Image("patrick")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 50, height: 50)
+                }
+            }
+            .frame(width: 300, height: 800)
+            .navigationTitle("My Menu Bar")
+        }
     }
+    
+}
+
+struct myImageView: View {
+    var body: some View {
+        // Here is how you display an image and scale it to fix a certain window size
+        Image("RedPanda")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300, height: 300)
+    }
+}
+
+struct mySoundView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Show image or text here")
+        }
+        .onAppear(perform: {
+            playAudioAsset("myTestSound")
+        })
+    }
+
+}
+
+var audioPlayer: AVAudioPlayer!
+
+// Here is a simple function to play a sound from Assets.xcassets
+func playAudioAsset(_ assetName : String) {
+   guard let audioData = NSDataAsset(name: assetName)?.data else {
+      fatalError("Unable to find asset \(assetName)")
+   }
+
+   do {
+      audioPlayer = try AVAudioPlayer(data: audioData)
+      audioPlayer.play()
+   } catch {
+      fatalError(error.localizedDescription)
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
